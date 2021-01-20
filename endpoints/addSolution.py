d3e6549acc2ba6048e353042246733287502ce3c
Samuel Lager <samuel.lager.43@gmail.com>
2021-01-20 17:33:23 +0100

    Fix bug with json float

--- endpoints/addSolution.py
+++ endpoints/addSolution.py
@@ -2,6 +2,7 @@ import utils.API_Responses as Responses
 import utils.Dynamo as Dynamo
 from botocore.exceptions import ClientError
 from datetime import datetime
+from decimal import Decimal
 import json
 
 def handler(event, context):
@@ -10,7 +11,7 @@ def handler(event, context):
             Item={
                 "PK": "SOLUTION",
                 "SK": datetime.now().isoformat(),
-                "JSON": json.loads(event['body'])
+                "JSON": json.loads(event['body'], parse_float=Decimal)
             }
         )
         return Responses._201()
